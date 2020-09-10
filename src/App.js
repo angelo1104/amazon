c039eb1c9badd62af5d34992cd612602b6979920
Angelo <madhav1004@icloud.com>
2020-09-10 23:09:31 +0530

    Complete header.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from "./Components/Header/Header";
-import Home from "./Components/Home/Home";
+import Header from "./Containers/Header/Header";
 
 function App() {
   return (
     <div className="App">
         <Header/>
-        <Home />
     </div>
   );
 }
